759d02cf2e665bfbf568b1952d96f12edd100092
Tejasave <tejasavebisen@gmail.com>
2024-09-01 16:24:16 +0530

    new

---
+++ max.pr.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+using namespace std;
+
+
+
+    int which_is_greater(int arr[],int n){
+    
+        int max;
+      max=arr[0];
+       
+        for(int i=1;i<n;i++){
+            if(arr[i]>max){
+            
+             max=arr[i];
+            }
+        }
+      return max;
+    } 
+    int main(){
+         int arr[4]={3,4,6,5};
+        int n = sizeof(arr) / sizeof(arr[0]);
+       
+     which_is_greater( arr, n);
+    
+    }
+    
+    
