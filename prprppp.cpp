cab2c41538755eec301d5cbe96add856d04d5d79
Tejasave <tejasavebisen@gmail.com>
2024-09-10 20:04:11 +0530

    new

---
+++ prprppp.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int length=0;
+	string s="Welcome ";
+	for(int i=1;i<s.length();i++){
+		length++;
+	}
+	cout<<length<<endl;
+	return 0;
+	
+}
