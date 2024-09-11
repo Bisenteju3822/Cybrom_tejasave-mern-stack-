0e788f81492a1ef580a93c6be9c47e4bdd21b7c9
Tejasave <tejasavebisen@gmail.com>
2024-09-11 17:16:34 +0530

    new it

---
+++ ptrp.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+ largest(int arr,int size,int *ptr){
+return *ptr;
+}
+int main(){
+int arr[5]={2,1,7,6,4};
+int size;
+int *ptr=&arr[0];
+
+
+	return 0;
+}
