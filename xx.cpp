766bd5b598a541dda393263d7836c504c0e9bc00
Tejasave <tejasavebisen@gmail.com>
2024-09-13 12:34:06 +0530

    new

---
+++ xx.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n=10;
+ for(int i=1;i<=n;i++){
+ 	if(i%2==0){
+ 		cout<<"even"<<endl;
+	 }
+	// else{
+	 //	cout<<"odd"<<endl;
+	//// }
+	 
+ }
+
+
+	return 0;
+}
