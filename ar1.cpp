122cc6a14a7867cb5d2eb13a69b556601d6219f2
Tejasave <tejasavebisen@gmail.com>
2024-09-14 17:49:17 +0530

    number sum

--- ar1.cpp
+++ ar1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
 	string str="Geeks for geeks";
 	 int length=0;
-	 for( int i=0;str[i]!='\0';i++){
+	 for( int str[i]-1;str[i]!='\0';i--){
 	 	length++;
 	 
 	 }
---
+++ prxx22.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+int c=0;
+cin>>n;
+while(n>0){
+  int x=n%10;
+  c=c+x;
+  n=n/10;
+  
+  	
+  	
+}
+cout<<c<<endl;
+
+
+
+	return 0;
+}
