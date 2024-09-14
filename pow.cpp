0dcf04c159da22ebb70905e452c4e0ca7aa3da1f
Tejasave <tejasavebisen@gmail.com>
2024-09-14 15:14:53 +0530

    new

---
+++ pow.cpp
@@ -0,0 +1,21 @@
+//Program of power 
+#include<iostream>
+using namespace std;
+int main(){
+	int c;
+	int n;
+	int r=1;
+	cin>>n;
+	cin>>c;
+	while(n>0){
+		r=r*c;//1*3
+		n--;
+	}
+	cout<<r<<endl;	 
+		 
+	
+
+
+
+	return 0;
+}
