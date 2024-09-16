3a0f95cdf1586e4f7c8d0d96fb03f44962b3351d
Tejasave <tejasavebisen@gmail.com>
2024-09-16 12:32:15 +0530

    new

---
+++ strpr.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+//#include<string>
+//#include<algorithm>
+using namespace std;
+int main(){
+string s="TejasaveBisen";
+//int length=0;
+//int reverse;
+for(int i=s.length()-1;i>0;i--){
+   
+	cout<<s[i];
+}
+  //reverse(s.begin(),s.end());
+//	cout<<s<<endl;
+
+
+//}//
+
+
+	return 0;
+}
