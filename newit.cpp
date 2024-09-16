f7e1dc0e69b43d7c746e585bc7403283c1e17e9d
Tejasave <tejasavebisen@gmail.com>
2024-09-16 14:37:43 +0530

    new it

---
+++ newit.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int n;
+ cin>>n;
+ int count =0;
+ int i=1;
+ while( i<n){
+ 	 if(i*i==n){
+ 	 //	cout<<i;
+ 	// 	break;
+ 	//cout<<"iteration"<<i+1<<endl;
+ 	 	i++;
+
+	  }
+	  cout<<i<<endl;
+	  
+	 // cout<<<<endl;
+	  i++;
+ }
+
+
+	return 0;
+}
