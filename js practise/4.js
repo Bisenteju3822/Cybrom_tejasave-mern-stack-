a486439d30529b319fac8b95c387d1346a217a12
Tejasave <tejasavebisen@gmail.com>
2024-09-16 18:31:51 +0530

    new

--- js practise/4.js
+++ js practise/4.js
@@ -9,6 +9,28 @@
 
 }
 console.log(description);*/
-const names=['teju','pallu',1,[3,2]]
+/*const names=['teju','pallu',1,[3,2]]
  const[i, ,,j]=names
-console.log(i,j)
+console.log(i,j)*/
+ const description={
+    firstname:"teju",
+    Surename:"Bisen",
+    age:22,
+    friends:['ritesh','shivam','subhash','shreyansh','aman'],
+     girlfriends:["null","null",'null',"null"],
+
+
+
+ }
+ const  {
+    firstname:name,
+    Surename:end,
+    age:umr,
+    friends:f,
+
+ }=description
+ console.log(f,umr)
+
+ 
+
+ 
