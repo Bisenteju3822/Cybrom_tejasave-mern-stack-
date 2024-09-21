972e57a5b3b271259dc312de1bca9ce65eb784cd
Tejasave <tejasavebisen@gmail.com>
2024-09-21 19:56:17 +0530

    new

---
+++ ladder3.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+	int n,t,s,c,d,f,g,i,z,w,h,e,j,k,b,l,m,q;
+
+	cin>>n;
+   	c=n/500;
+    t=n%500;
+    
+    cout<<c<<endl;
+    cout<<t<<endl;
+    s=t/200;
+    cout<<s<<endl;
+    d=t%200;
+    cout<<d<<endl;
+    f=d/100;
+    cout<<f<<endl;
+    g=d%100;
+    cout<<g<<endl;
+    e=g/50;
+    cout<<e<<endl;
+    i=g%50;
+    cout<<i<<endl;
+    z=i/20;
+    cout<<z<<endl;
+    w=i%20;
+    cout<<w<<endl;
+    h=w/10;
+    cout<<h<<endl;
+    j=w%10;
+    cout<<j<<endl;
+    k=j/5;
+    cout<<k<<endl;
+    b=j%5;
+    cout<<b<<endl;
+    l=b/2;
+    cout<<l<<endl;
+    m=b%2;
+    cout<<m<<endl;
+    q=m/1;
+    cout<<q<<endl;
+    if(c>0){
+    	cout<<"Total 500 rs="<<c<<endl;
+	}
+	 if(s>0){
+		cout<<"Total 200 rs="<<s<<endl;
+	}
+	
