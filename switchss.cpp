86073b1347640c65fcab49c2441ad82eff321b68
Tejasave <tejasavebisen@gmail.com>
2024-09-24 17:16:06 +0530

    new

---
+++ switchss.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int c;
+ cout<<"It take float or not";
+ cin>>c;
+ switch (c){
+ 	case 5+5:{
+		
+	 
+ 		cout<<"hello"<<endl;
+	     break;
+	 }
+	 
+	 case 10*10 :{
+		
+	 
+	 	cout<<"bold"<<endl;
+		break;
+	}
+		
+		default:{
+			
+		
+	    cout<<"invalid"<<endl;
+	    break;
+	}
+	 
+ }
+
+
+	return 0;
+}
