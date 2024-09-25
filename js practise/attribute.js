0be8820fce4fc1a1410f5125358b9658d7fdfdf5
Tejasave <tejasavebisen@gmail.com>
2024-09-25 12:22:16 +0530

    new

---
+++ js practise/attribute.js
@@ -0,0 +1,9 @@
+function run(){
+    let selectphoto=document.querySelector('#p1')
+    selectphoto.src="1157289.jpg"
+}
+function leave(){
+    let leavephoto=document.querySelector('#p1')
+    selectphoto.style.color=black
+
+}
