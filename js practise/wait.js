0728c513d173db4e31410cf1fd651f72af286236
Tejasave <tejasavebisen@gmail.com>
2024-09-26 12:22:56 +0530

    new

---
+++ js practise/wait.js
@@ -0,0 +1,10 @@
+function stop(){
+    let a=document.querySelector("#name")
+    let name=a.value
+    let b=document.querySelector("#value")
+    let age=b.value
+    console.log(name)  
+    console.log(age)
+    
+    return false
+}
