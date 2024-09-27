ae96753a04d0fbda3f97d182236950ede79bdf6d
Tejasave <tejasavebisen@gmail.com>
2024-09-27 12:38:57 +0530

    new

---
+++ js practise/22.js
@@ -0,0 +1,4 @@
+function img1(){
+    const a=document.querySelector("#long")
+    a.scr="61J9KUops4L._SX679_.jpg"
+}
--- js practise/pr44.js
+++ js practise/pr44.js
@@ -1,12 +1,52 @@
 function fun(){
-    const b=document.querySelector("#t1")
-     let name=b.value
+    const b=document.querySelector("#t1").value
     const c=document.querySelector("#t2").value
     const d=document.querySelector("#t3").value
     const e=document.querySelector("#t4").value
     const f=document.querySelector("#t5").value
-    console.log(name)
-    
+    console.log(b,c,d,e,f)
    
+  if(b ===""){
+    document.querySelector("#error").innerHTML="Enter your name"
+     let selname=document.querySelector("#t1")
+     selname.style.borderColor="red"
+     selname.style.outlineColor="red"
+    
+     
+
+  }
+   if(c===""){
+    document.querySelector("#error").innerHTML="Enter your name"
+     let name1=document.querySelector("#t2")
+     name1.style.borderColor="red"
+     name1.style.outlineColor="red"
+     
+     
+  }
+  if(d===""){
+    document.querySelector("#error").innerHTML="Enter your name"
+    let name2=document.querySelector("#t3")
+    name2.style.borderColor="red"
+    name2.style.outlineColor="red"
+    
+    
+  }
+  if(e===""){
+    document.querySelector("#error").innerHTML="Enter your name"
+     let name3=document.querySelector("#t4")
+     name3.style.borderColor="red"
+     name3.style.outlineColor="red"
+     
+     
+  }
+  if(f===""){
+    document.querySelector("#error").innerHTML="Enter your n"
+     let name4=document.querySelector("#t5")
+     name4.style.borderColor="red"
+     name4.style.outlineColor="red"
+     
+     
+  }
+
 
 }
