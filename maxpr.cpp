de10f359c9702920de3538b71404696219872d21
Tejasave <tejasavebisen@gmail.com>
2024-09-28 12:18:08 +0530

    new

---
+++ maxpr.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int arr[5]={2,3,4,-1,5};
+   int max=arr[0];
+   int min=arr[0];
+   int replace=arr[0];
+   
+   for(int i=1;i<=arr[i];i++){
+   	if(arr[i]>max)
+{
+	max=arr[i];
+	   }	   
+   }
+   cout<<"max value"<<max<<endl;
+   for(int j=1;j<=arr[j];j++){
+   
+   if(arr[j]<min){
+   	min=arr[j];
+   }
+  }
+      cout<<"min value"<<min<<endl;
+
+
+	
+
+cout<<replace<<endl;
+	return 0;
+}
