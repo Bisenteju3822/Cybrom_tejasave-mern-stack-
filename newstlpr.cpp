9aabe19ca6bbab8d13a21074e0189a9b17cd4138
Tejasave <tejasavebisen@gmail.com>
2024-10-01 10:20:22 +0530

    new.cpp

---
+++ newstlpr.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+void explain(){
+	pair<int,int> p={1,2};
+	cout<<p.first<<""<<p.second;
+	pair<int,pair<int,int>> p1={1,{1,2}};
+ cout<<p.first<<""<<p1.second.second<<p1.second.first;
+ 	pair<int,int> arr[]={{2,3},{3,4},{2,3}};
+ 	cout<<arr[1].second;
+}
