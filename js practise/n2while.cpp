b585802e541c3c985011da173bbc4b2e78ad49da
Tejasave <tejasavebisen@gmail.com>
2024-10-02 14:40:48 +0530

    new

---
+++ js practise/n2while.cpp
@@ -0,0 +1,18 @@
+//Hello sir i am tejasasve recent graduate eith bachelor degree in 
+#include<iostream>
+using namespace std;
+int main(){
+	int n=5;
+	int i=1;
+while( i<=20){
+	if(i%5==0){
+  		cout<<i<<endl;
+		
+		
+	}
+	i++;
+}
+
+
+	return 0;
+}
