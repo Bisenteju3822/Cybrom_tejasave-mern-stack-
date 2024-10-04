d252663572efbb5e7e7d397f2b31cbe307566815
Tejasave <tejasavebisen@gmail.com>
2024-10-04 20:44:31 +0530

    new

---
+++ prswi.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int n=4;
+int c;
+cin>>c;
+switch(c){
+	case 1:{
+		cout<<"n";
+		continue;
+		
+	}
+
+		default :{
+			cout<<"bb";
+			break;
+		}
+	
+	}
+	return 0;
+}
