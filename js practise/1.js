5ef117e481d5a22c205ed826bb4ba2bbe33a2393
Tejasave <tejasavebisen@gmail.com>
2024-10-04 12:56:41 +0530

    new

--- js practise/1.js
+++ js practise/1.js
@@ -1,4 +1,20 @@
-const teju="new"
- function n1{
-const  teju="new"
+/*setTimeout(() => {
+    console.log("hello")
+    
+}, 2000)*/
+/*setTimeout(function() {
+     console.log("new")
+}, 4000);*/
+
+const shw=function(){
+    let a=document.querySelector("#website")
+     let b=document.querySelector("#img")
+     a.style.display="block"
+     b.style.display="none"
 }
+const ondone=function(){
+    setTimeout(function() {
+        
+   }, 4000)
+}
+
