95a8554784f2992a0eaccbb93b520b7c2f31cde8
Tejasave <tejasavebisen@gmail.com>
2024-10-06 23:23:48 +0530

    new

---
+++ js practise/pft.js
@@ -0,0 +1,14 @@
+
+/*const a=parseInt(prompt("Enter your number"))
+const b=parseInt(prompt("Enter your number"))
+const c=parseInt(prompt("Enter your number"))
+
+
+
+const avr=(a+b+c)/3
+
+a*/const change=function(){
+    let b=document.querySelector("#change")
+    b.src="dataScience.36dd03b9.jpg"
+    b.style.width="500px"
+}
