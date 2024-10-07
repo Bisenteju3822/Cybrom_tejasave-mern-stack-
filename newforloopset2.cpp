bf669d085d2a0532591114d2282a721c1d439e64
Tejasave <tejasavebisen@gmail.com>
2024-10-07 20:53:58 +0530

    new

---
+++ newforloopset2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+int firstnumber=n;
+int lastdigit;
+lastdigit=n%10;
+int sum;
+for(firstnumber;firstnumber>=10;firstnumber/=10){
+	
+	
+}
+cout<<"lastnumber"<<lastdigit<<endl;
+cout<<"firstnumber"<<firstnumber<<endl;
+
+sum=lastdigit+firstnumber;
+
+ cout<<sum<<endl;
+ 
+
+	return 0;
+}
