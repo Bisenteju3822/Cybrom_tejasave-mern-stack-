02c8371e98669c28da632efc520ad0de9d9ae4ab
Tejasave <tejasavebisen@gmail.com>
2024-10-07 17:25:51 +0530

    new

--- js practise/pft2.js
+++ js practise/pft2.js
@@ -1,25 +1,28 @@
-const count=0;
-const slider=function(){
-      setInterval(run,3000)
+let count = 0;  // Use 'let' since 'count' will be incremented
+const slid = function() {
+  setInterval(run, 3000);
 
-      function run(){
-        if(count===0){
-        let selectarticle=document.querySelector("sldierdiv")
-        selectarticle.style.marginLeft="0px"
-        selectarticle.transition="margin-left .5s"
-        count++;
-        }
-        if(count===1){
-            let selectarticle=document.querySelector("sldierdiv")
-            selectarticle.style.marginLeft="-1000px"
-            selectarticle.transition="margin-left .5s"
+    function run() {
+        let selectarticle = document.querySelector("#sliderdiv");  // Use '#' for ID selector
+
+        if (count === 0) {
+            selectarticle.style.marginLeft = "0px";
+            selectarticle.style.transition = "margin-left .5s";
             count++;
-            }
-            if(count===2){
-                let selectarticle=document.querySelector("sldierdiv")
-                selectarticle.style.marginLeft="-2000px"
-                selectarticle.transition="margin-left .5s"
-                count==0
-                }
-      }
+        } else if (count === 1) {
+            selectarticle.style.marginLeft = "-1000px";
+            selectarticle.style.transition = "margin-left .5s";
+            count++;
+        } else if (count === 2) {
+            selectarticle.style.marginLeft = "-2000px";
+            selectarticle.style.transition = "margin-left .5s";
+            count = 0;  // Reset the counter correctly
+        }
+     
+    }
+    run();
+    alert("on");
 }
+
+window.onload = slid;
+
