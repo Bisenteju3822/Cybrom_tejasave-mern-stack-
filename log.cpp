9c56ecd2cc514d0f523303dbc851e887a58715cd
Tejasave <tejasavebisen@gmail.com>
2024-10-08 17:58:07 +0530

    new

---
+++ log.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+      int lastn=log10(n)+1;
+      cout<<lastn<<endl;
+    return 0;
+}
