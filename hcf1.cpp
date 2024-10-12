f935442ff7d85e0c15f1bace148d3d98ccc6ce7b
Tejasave <tejasavebisen@gmail.com>
2024-10-12 17:37:01 +0530

    new

--- hcf1.cpp
+++ hcf1.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int gcd(int x, int y) {
+int gcd(int x, int y)
+{
     if (y == 0)
         return x;
     return gcd(x, x % y);
 }
 
-int main() {
+int main()
+{
     int x = 24, y = 12;
     cout << "The HCF of " << x << " and " << y << " is: " << gcd(a, b) << endl;
     return 0;
