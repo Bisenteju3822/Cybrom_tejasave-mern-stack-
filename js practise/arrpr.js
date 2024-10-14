004824e37131c65f21d2479d02549c5c3c3c882e
Tejasave <tejasavebisen@gmail.com>
2024-10-14 20:55:06 +0530

    new

---
+++ js practise/arrpr.js
@@ -0,0 +1,29 @@
+/*const arr = [
+  2, 3, 4, 5, 6,
+  "Hello",
+  function () {
+    console.log("function run")
+  },
+  6 < 8
+  // Uncomment the next line if `a` and `b` are defined
+  // a != b
+];
+
+console.log(typeof (arr)); // "object"
+arr[6](); // Calls the function within the array
+console.log(arr[5][3])
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i])
+}
+for (let i in arr) {
+  console.log(arr[i])
+}
+for (let value of arr) {
+  console.log(arr[value])
+}*/
+const string1 = "hello teju"
+const string2 = string(string4)
+consolelog(string2)
+const string = function (str) {
+  return str.split('').reverse().join('')
+}
