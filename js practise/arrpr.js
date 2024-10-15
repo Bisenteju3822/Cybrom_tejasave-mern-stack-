68c851ac5fe01330066881bee428d7f8164d9642
Tejasave <tejasavebisen@gmail.com>
2024-10-15 14:54:31 +0530

    new

--- js practise/arrpr.js
+++ js practise/arrpr.js
@@ -1,3 +1,7 @@
-const arr = [22, 44, 55, 77, "hello"];
+/*const arr = [22, 44, 55, 77, "hello"];
 arr.splice(2, 2, "teju", "pallu")
-console.log(arr)
+console.log*/
+let arr = [2, 3, 4, 5, "hello"]
+for (let value in arr) {
+  console.log(value)
+}
---
+++ js practise/new.js
@@ -0,0 +1,6 @@
+let arr = {
+  name: "teju", last: pallav, age: 22
+}
+for (let key in arr) {
+  console.log(key, arr[key])
+}
