e6049ee3f2cae847ec61ef453e3fd955c6899c7b
Tejasave <tejasavebisen@gmail.com>
2024-10-19 20:17:48 +0530

    new

---
+++ for nested loop pattern all.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+using namespace std;
+/*int main(){
+  int n=3;
+  for(int i=1;i<=n;i++){
+    for(int j=1;j<=i;j++){
+      cout<<" "<<"*"<<" ";
+    }
+    cout<<endl;
+  }
+  return 0;//output *
+                    * *
+                    * * *
+}*/
+//
+
+/*int main(){
+  int n=3;
+  for(int i=1;i<=n;i++){
+    for(int j=1;j<=i;j++){
+      cout<<i;
+    }
+    cout<<endl;//output 1
+                        2 2
+  }                       3 3 3
+
+  return 0;
+}*/
+/*int main(){
+    int n=3;
+  for(int i=1;i<=n;i++){
+    for(int j=1;j<=i;j++){
+      cout<<j;
+}
+cout<<endl;
+}
+return 0;
+}*/
+/*int main() {
+    int height = 3;
+   int num=1;
+    for (int i = 1; i <= 3; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout <<num;
+                                num++; output1
+                                             2 3
+                                             4 5 6
+
+        }
+
+        cout << endl;
+    }
+return 0;
+} */
+/*int main() {
+    int height = 3;
+   int num=1;
+    for (int i = 1; i <= 3; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout <<" "<<num*num<<" ";
+               num++;      output 1
+                                  4 9
+                                  16 25 36
+
+        }
+
+        cout << endl;
+    }
+}*/
+/*int main() {
+    int n=3;
+    for (int i = 1; i <= 3; i++) {
+        for (int j = 1; j <= i; j++) {
+        	    cout<<" "<<j*j<<" ";
+
+            
+			   
+                       output 1
+                              1 4
+                              1 4 9
+        }
+        cout<<endl;
+
+        cout << endl;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
