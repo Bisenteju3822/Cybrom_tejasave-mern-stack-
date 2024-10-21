6593d1d95c58ae21f372b548c3018d556016f1fd
Tejasave <tejasavebisen@gmail.com>
2024-10-21 12:13:03 +0530

    new

---
+++ newobex.js
@@ -0,0 +1,24 @@
+/*const information = {
+  Name: "teju",
+  age: 22,
+  value: 9,
+  class: "A"
+}
+console.log(information)*/
+const arr = [
+  {
+    name: "teju",
+    surname: "Bisen"
+  },
+  {
+    name1: "pallu",
+    surname: "Bisen"
+  },
+  {
+    name2: "hiya",
+    surname: "bisen"
+  },
+
+
+]
+console.log(arr[0], arr[1])
