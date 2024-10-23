80057c2cadb5e44a71ab7e4b8ae8c82b86dd2999
Tejasave <tejasavebisen@gmail.com>
2024-10-23 11:23:18 +0530

    new

---
+++ project card/objdis.js
@@ -0,0 +1,11 @@
+const dis = {
+  name: "teju",
+  age: 22,
+  height: 5.7,
+  sum: function () {
+    return "happy"
+  }
+}
+dis.age
+const { name, age, height, sum } = dis
+console.log(name, age, height, sum)
