237e95853a28fcdde997aa6566f6b0a727b97335
Tejasave <tejasavebisen@gmail.com>
2024-10-23 11:37:21 +0530

    new

---
+++ project card/function.js
@@ -0,0 +1,14 @@
+function sum() {
+
+}//a normal function
+const sum2 = function () {
+
+}// function expression
+const sum1 = () => {
+
+}//arrow function
+//imediately invoked function expression
+(function (a, b) {
+  return a + b
+})(3, 2)
+console.log(7 + 5)
