c2602bc70731f4e62213301de9069871ab1caddf
Tejasave <tejasavebisen@gmail.com>
2024-11-17 11:29:05 +0530

    new

--- module1.js
+++ module1.js
@@ -1,2 +1,4 @@
-import boy from './module2.js'
-console.log(boy(3, 2))
+import { add, sub } from './module3.js';
+
+console.log(add(3, 4));  // Output: 7
+console.log(sub(3, 4));  // Output: -1
---
+++ module3.js
@@ -0,0 +1,7 @@
+function add(a, b) {
+  return a + b
+}
+function sub(s, x) {
+  return s - x
+}
+export { add, sub }
