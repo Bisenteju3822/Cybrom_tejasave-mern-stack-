287df46ab5fa7bd4185ccbf67344ff96753051b4
Tejasave <tejasavebisen@gmail.com>
2024-11-19 09:38:51 +0530

    new

---
+++ prtwodimodd.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int arr[2][3];
+  for (int i = 0; i < 2; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      cin >> arr[i][j];
+    }
+  }
+  for (int i = 0; i < 2; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      if (arr[i][j] % 2 != 0)
+      {
+        cout << arr[i][j] << "  == odd number," << " ";
+      }
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
