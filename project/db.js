405ace0f633409bfb05685cd83aa53e91aa55ed6
Tejasave <tejasavebisen@gmail.com>
2024-11-20 16:56:55 +0530

    new

--- project/db.js
+++ project/db.js
@@ -1,13 +1,28 @@
 async function okey() {
-  let a = await fetch("http://localhost:3000/student")
-  let b = await a.json();
-  let c = b.map((e) => `
- <h1>${e.id}</h1>
- <h1>${e.name}</h1>
- <h1>${e.age}</h1>
- <h1>${e.address}</h1>`
+  let first = await fetch("http://localhost:3000/student")
+  let second = await first.json()
+  let third = second.map((e) =>
+    `<tr>
+   <td>${e.name}</td>
+      <td>${e.age}</td>
+        <td>${e.address}</td>
+   <td>${e.id}</td> 
+   <td><button onclick="mydelete(${e.id})">Delete</button></td>
+  </tr>`
   ).join("")
-  document.querySelector("#n1").innerHTML = c
-  console.log(c)
+  document.querySelector("#showdata").innerHTML = third
+
+
+
 }
 okey()
+function mydelete() {
+  fetch(`http://localhost:3000/student/${id}`, {
+    /**
+     * 
+     */
+    method: "DELETE"
+  })
+    .then(first => alert("it suceesfully delete"))
+}
+mydelete()
