a681795530258e99fee4fdea804ae042d48c162a
Tejasave <tejasavebisen@gmail.com>
2024-11-23 13:01:26 +0530

    new

---
+++ prtreedim.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int arr[2][2][2];
+
+  // Input elements in the 3D array
+  for (int i = 0; i < 2; i++)
+  {
+    for (int j = 0; j < 2; j++)
+    {
+      for (int k = 0; k < 2; k++)
+      {
+        cin >> arr[i][j][k];
+      }
+    }
+  }
+
+  // Print the elements of the 3D array
+  cout << "Array elements are:" << endl;
+  for (int i = 0; i < 2; i++)
+  {
+    for (int j = 0; j < 2; j++)
+    {
+      for (int k = 0; k < 2; k++)
+      {
+        cout << arr[i][j][k] << " ";
+      }
+      cout << endl;
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
