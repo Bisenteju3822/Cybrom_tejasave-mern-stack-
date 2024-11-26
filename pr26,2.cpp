3724f1f0562c536ed056a00d4302497f8f0afc7e
Tejasave <tejasavebisen@gmail.com>
2024-11-26 15:00:54 +0530

    new

---
+++ pr26,2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+  int arr[5] = {1, 2, 3, 4, 5};
+  std::cout << arr[10] << std::endl; // Error: accessing out-of-bounds element
+  return 0;
+}
+#include <iostream>
+
+int main()
+{
+  int arr[5] = {1, 2, 3, 4, 5};
+  std::cout << arr[10] << std::endl; // Error: accessing out-of-bounds element
+  return 0;
+}
+#include <iostream>
+
+int main()
+{
+  int arr[5] = {1, 2, 3, 4, 5};
+  std::cout << arr[10] << std::endl; // Error: accessing out-of-bounds element
+  return 0;
+}
