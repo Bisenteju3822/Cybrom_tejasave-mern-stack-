c41c465560660122a56adf16bd2300a2fb118ba8
Tejasave <tejasavebisen@gmail.com>
2024-11-30 10:03:03 +0530

    new

--- Project-admin-Dashboard/admin.js
+++ Project-admin-Dashboard/admin.js
@@ -1,7 +1,5 @@
-// script.js
-
+// Set light mode
 document.getElementById('light-mode-btn').addEventListener('click', function () {
-  // Set light mode
   document.body.classList.add('light-mode');
   document.body.classList.remove('dark-mode');
 
@@ -9,8 +7,8 @@ document.getElementById('light-mode-btn').addEventListener('click', function () 
   localStorage.setItem('mode', 'light');
 });
 
+// Set dark mode
 document.getElementById('dark-mode-btn').addEventListener('click', function () {
-  // Set dark mode
   document.body.classList.add('dark-mode');
   document.body.classList.remove('light-mode');
 
@@ -18,4 +16,13 @@ document.getElementById('dark-mode-btn').addEventListener('click', function () {
   localStorage.setItem('mode', 'dark');
 });
 
-// Check the saved mode on page load and apply i
+// Redirect to login page
+
+
+// Check the saved mode on page load and apply it
+window.addEventListener('DOMContentLoaded', (event) => {
+  const savedMode = localStorage.getItem('mode');
+  if (savedMode) {
+    document.body.classList.add(savedMode === 'dark' ? 'dark-mode' : 'light-mode');
+  }
+});
