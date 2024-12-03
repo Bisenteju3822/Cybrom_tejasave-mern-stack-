741985bd4774d1214828bf115fcd0b07083cb443
Tejasave <tejasavebisen@gmail.com>
2024-12-03 13:19:51 +0530

    new

---
+++ prgreater3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int greater()
+{
+
+  // a=2,b=3 a=a+b;a=5;b=a-b;a=a-b;
+  return max(a, b);
+}
+int main()
+{
+  int a, b;
+  cout << "Enter A" << endl;
+  cin >> a;
+  cout << "Enter B" << endl;
+  cin >> b;
+  cout << "greater===" << swap(a, b);
+
+  return 0;
+}
