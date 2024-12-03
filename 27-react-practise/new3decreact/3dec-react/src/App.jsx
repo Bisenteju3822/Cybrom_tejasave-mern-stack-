b26ba8ee105ae312ce5bee86544c78d99d7afa86
Tejasave <tejasavebisen@gmail.com>
2024-12-03 12:29:56 +0530

    new

---
+++ 27-react-practise/new3decreact/3dec-react/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css"
+function App(){
+   let st={
+    backgroundColor:"yellow",
+    color:"black"
+
+   }
+  return (
+    <>
+
+    <h1 style={{backgroundColor:"red",color:"white"}}>we ave to prepare for this </h1>
+     <h3 style={st}>We are to decide this</h3>
+     <h4 className="practise">new boy</h4>
+    </>
+  )
+}
+
+export default App
