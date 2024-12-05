ec353c8280fa3a7a2171fef69587189df1cb139f
Tejasave <tejasavebisen@gmail.com>
2024-12-05 14:45:14 +0530

    new

--- 27-react-practise/new3decreact/3dec-react/src/App.jsx
+++ 27-react-practise/new3decreact/3dec-react/src/App.jsx
@@ -1,20 +1,14 @@
 import "./App.css";
-import Nav from './Nav';
-
 function App() {
-    let st = {
-        backgroundColor: "yellow",
-        color: "black"
-    };
+  
 
     return (
         <>
-            <Nav />
-            <h1 style={{backgroundColor: "red", color: "white"}}>We have to prepare for this</h1>
-            <h3 style={st}>We are to decide this</h3>
-            <h4 className="practise">new boy</h4>
+            <Head></Head>
+          
         </>
     );
 }
+import Head from "./Header";
 
 export default App;
---
+++ 27-react-practise/new3decreact/3dec-react/src/Header.jsx
@@ -0,0 +1,23 @@
+const Head=()=>{
+  return(
+    <>
+   <section className="top">
+    <ul  className="hero">
+    <li>Home</li>
+    <li>About</li>
+    <li>Service</li>
+    <li>login</li>
+    </ul>
+    <div>
+      <h1>
+        HERO SECTION
+      </h1>
+    </div>
+   </section>
+
+</>
+  )
+
+  
+}
+export default Head
